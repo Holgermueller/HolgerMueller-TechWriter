0b2917e4527360cc7d888bf5a5b44d462d593d44
holgermueller <9jholger@gmail.com>
2024-08-12 15:50:38 -0500

    got html file on dom

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const HTTP = require("http");
+const fs = require("fs");
 const PORT = 8080;
 
 HTTP.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.write("Hello World! Path hit: " + req.url);
-  res.end();
+  fs.readFile("index.html", (err, data) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(data);
+    return res.end();
+  });
 }).listen(PORT, () => {
   console.log("Server listening on Port: " + PORT);
 });
