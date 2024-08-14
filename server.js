ee63cb0b9ab0b85ed9cfbb0d454595c5ae79a38e
holgermueller <9jholger@gmail.com>
2024-08-14 12:03:30 -0500

    worked on server a little more

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const HTTP = require("http");
 const fs = require("fs");
+
 const PORT = 8080;
 
 HTTP.createServer((req, res) => {
   fs.readFile("index.html", (err, data) => {
+    if (err) console.log(err);
     res.writeHead(200, { "Content-Type": "text/html" });
     res.write(data);
     return res.end();
